46548715614adfb5b8ed937905fcbe2e2c086147
Jobson de Oliveira Coutinho <jobsonoliveira007@gmail.com>
2024-08-31 22:23:19 -0300

    Header e poster de apresentação adicionados, bootstrap adicionado ao projeto, sidebar funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './App.css'
+import Header from './components/header/Header';
+import ImgApresentacao from './assets/imgDeConvite.svg'
 
 function App() {
   return (
-    <div>Página inicial</div>
+    <main className='sh-container'>
+      <Header />
+
+      <section className="sh-apresentacao">
+        <article className="sh-apresentacao-container">
+          <img src={ImgApresentacao} alt="" className="sh-apresentacao-img" />
+        </article>
+        <article className="sh-apresentacao-item">
+          <h2 className="sh-apresentacao-title">Oque você procura está aqui!</h2>
+          <p className="sh-apresentacao-text">Economize o seu tempo, seja cliente e contrate já um profissional</p>
+          <a href="#" className='sh-apresentacao-link'>Cadastre-se</a>
+        </article>
+      </section>
+    </main>
+
   );
 }
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './header.css'
+import Logo from '../../assets/logo.png';
+
+// ---- Bootstrap
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Options from '../../assets/options.png'
+
+const Header = () => {
+    // ---- Const Bootstrap
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <section className="sh-header">
+            <div className="d-lg-none sh-header-options">
+                <div className="sh-options-itens">
+                    <img src={Options} alt="Opções de navegação" className='sh-options-img' onClick={handleShow} />
+                </div>
+                <div className="sh-options-itens">
+                    <img src={Logo} alt="Logo da SkillHub" className='sh-logo-img' />
+                </div>
+            </div>
+
+            <div className="d-none d-lg-block sh-desktop-logo">
+                <img src={Logo} alt="Logo da SkillHub" className='sh-logo-img' />
+            </div>
+
+            <Offcanvas show={show} onHide={handleClose} responsive="lg" className='sh-header-offcanvas'>
+                <Offcanvas.Header closeButton className='sh-offcanvas-element'>
+                    <Offcanvas.Title className='sh-lg-header-logo'>
+                        {/* <img src={Logo} alt="Logo da SkillHub" className='sh-logo-img' /> */}
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ul className="mb-0 sh-header-navigation">
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Planos</p>
+                        </li>
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Vantágens</p>
+                        </li>
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Como começar</p>
+                        </li>
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Sobre nós</p>
+                        </li>
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Dúvidas</p>
+                        </li>
+                        <li className="sh-navigation-item">
+                            <p className='sh-item-text'>Login</p>
+                        </li>
+                    </ul>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </section>
+    )
+}
+
+export default Header;
+
+// ----------------------------------------
+
