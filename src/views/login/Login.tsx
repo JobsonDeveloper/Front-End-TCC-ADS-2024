f98c25bd9a6493d783154c1c5f5c68bc0b1026e6
Jobson Oliveira <jobsondeveloper@gmail.com>
2024-10-12 16:24:16 -0300

    inputs do Material UI implementados

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -7,6 +7,8 @@ import logo from '../../assets/icons/logoLogin.svg';
 import imgDescricao from '../../assets/icons/descricao.svg';
 import imgLogin from '../../assets/icons/btn-login.svg';
 import Loading from "../../components/loading/Loading";
+import { Box, Button, ButtonBase, FilledInput, FormControl, FormHelperText, IconButton, Input, InputAdornment, InputLabel, OutlinedInput, SxProps, TextField, Theme } from "@mui/material";
+import { Edit, Visibility, VisibilityOff } from "@mui/icons-material";
 
 const Login = () => {
     const [removeLoading, setRemoveLoading] = useState(false);
@@ -37,18 +39,18 @@ const Login = () => {
             .then((data) => {
                 console.log(data);
 
-                if(data.status === 201) {
-                   localStorage.setItem('usuário', data.data.Usuarioid);
-                    
-                   // Se o usuário é um cliente
-                   if(data.data.type === 0) {
-                    pagina('/home-cliente');
-                   }
+                if (data.status === 201) {
+                    localStorage.setItem('usuário', data.data.Usuarioid);
 
-                   // Se o usuário é um freelancer
-                   if(data.data.type === 1) {
-                    pagina('/home-freelancer');
-                   }
+                    // Se o usuário é um cliente
+                    if (data.data.type === 0) {
+                        pagina('/home-cliente');
+                    }
+
+                    // Se o usuário é um freelancer
+                    if (data.data.type === 1) {
+                        pagina('/home-freelancer');
+                    }
                 }
 
                 setRemoveLoading(true);
@@ -60,6 +62,18 @@ const Login = () => {
             setRemoveLoading(true);
         }, 2000);
     }, []);
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
 
     return (
         <section className="sh-login">
@@ -79,13 +93,11 @@ const Login = () => {
 
                 <li className="sh-formulario-item sh-formulario-main">
                     <div className="sh-formulario-main-content">
-                        <p className="sh-main-dado-titulo">CPF/CNPJ</p>
-                        <input type="number" className="sh-main-input" value={identificador} onChange={mudarIndentificador} />
+                        <TextField id="outlined-basic" label="CPF/CNPJ" variant="outlined" className="sh-formulario-data-text" />
                     </div>
 
                     <div className="sh-formulario-main-content">
-                        <p className="sh-main-dado-titulo">Senha</p>
-                        <input type="password" className="sh-main-input" value={senha} onChange={mudarSenha} />
+                        <TextField id="outlined-basic" label="Senha" variant="outlined" type="password" className="sh-formulario-data-text" />
                     </div>
                 </li>
 
